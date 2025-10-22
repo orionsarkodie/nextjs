36232083a74b666990dd79e7c92a22edc2039361
orionsarkodie <msark2001@gmail.com>
2025-10-22 07:58:57 +0000

    Catch-all segment

---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,9 @@
+export default function ReviewList(){
+    return(
+        <ul>
+            <li>This product is super!</li>
+            <li>I love how fast it was delivered.</li>
+            <li>Amazing product.</li>
+        </ul>
+    );
+}
